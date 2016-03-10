1f623219c20ab3220f52393b74af065285bfb26e
Alexander deCurnou <adecurnou@gmail.com>
2016-03-09 22:15:06 -0500

    Fixed RNG and closed cheat case

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("Welcome to Rock, Paper, Scissors!");
 
     loop {
-        let comp_throw = rand::thread_rng().gen_range(1, 3);
+        let comp_throw = rand::thread_rng().gen_range(0, 3);
 
         let rock_win = "Unfortunately, rock beats scissors. Try again.";
         let paper_win = "Unfortunately, paper beats rock. Try again.";
@@ -15,8 +15,8 @@ fn main() {
         let hum_win = "Congratulations, you've won!";
         let tie = "Darn, a tie! Try again.";
 
-        println!("Please decide upon your shape.");
-        println!("1 = Rock, 2 = Paper, 3 = Scissors");
+        print!("Please decide upon your shape: ");
+        print!("1 = Rock, 2 = Paper, 3 = Scissors\n");
 
         let mut hum_throw = String::new();
         io::stdin().read_line(&mut hum_throw)
@@ -27,38 +27,38 @@ fn main() {
         	Err(_)	=> continue,
         };
 
-        match comp_throw {
+        match hum_throw {
             1	=> {
-            	if hum_throw == 1 {
+            	if comp_throw == 1 {
             		println!("{}", tie);
-            	} else if hum_throw == 3 {
-            		println!("{}", rock_win);
+            	} else if comp_throw == 2 {
+            		println!("{}", paper_win);
             	} else {
             		println!("{}", hum_win);
             		break;
             	}
             }
             2	=> {
-            	if hum_throw == 1 {
-            	    println!("{}", paper_win);
-            	} else if hum_throw == 2 {
-            		println!("{}", tie);
+            	if comp_throw == 2 {
+            	    println!("{}", tie);
+            	} else if comp_throw == 3 {
+            		println!("{}", scissors_win);
             	} else {
             		println!("{}", hum_win);
             		break;
             	}
             }
             3	=> {
-            	if hum_throw == 2 {
-            		println!("{}", scissors_win);
-            	} else if hum_throw == 3 {
+            	if comp_throw == 1 {
+            		println!("{}", rock_win);
+            	} else if comp_throw == 3 {
             		println!("{}", tie);
             	} else {
             		println!("{}", hum_win);
             		break;
             	}
             }
-            _	=> {;}
+            _	=> {println!("Only numbers 1-3 please.");}
         }
     }
 }
